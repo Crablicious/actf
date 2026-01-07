2a9c3641d6e9549bcd33b811f6466a83728a5c0d
Adam Wendelin <adwe@live.se>
2026-01-07 15:33:09 +0100

    Space indent comments for error strings

--- error.c
+++ error.c
@@ -112,55 +112,55 @@ int eprependf(struct error *e, const char *fmt, ...)
 const char *actf_errstr(int rc)
 {
 	static const char *const strs[] = {
-		"not an error",	// ACTF_OK
-		"generic error",	// ACTF_ERROR
-		"internal error",	// ACTF_INTERNAL
-		"out of memory",	// ACTF_OOM
-		"property was not found",	// ACTF_NOTFOUND
-		"the json could not be parsed",	// ACTF_JSON_PARSE_ERROR
-		"incompatible json value",	// ACTF_JSON_ERROR
-		"incompatible type of json value",	// ACTF_JSON_WRONG_TYPE
-		"a value is not greater or equal than zero",	// ACTF_JSON_NOT_GTEZ
-		"a value is not greater than zero",	// ACTF_JSON_NOT_GTZ
-		"an alignment is not a power of two",	// ACTF_INVALID_ALIGNMENT
-		"non-existent byte order",	// ACTF_INVALID_BYTE_ORDER
-		"non-existent bit order",	// ACTF_INVALID_BIT_ORDER
-		"invalid range",	// ACTF_INVALID_RANGE
-		"invalid range set",	// ACTF_INVALID_RANGE_SET
-		"invalid uuid",	// ACTF_INVALID_UUID
-		"invalid mapping",	// ACTF_INVALID_MAPPING
-		"invalid field location",	// ACTF_INVALID_FLD_LOC
-		"invalid field class",	// ACTF_INVALID_FLD_CLS
-		"invalid flags",	// ACTF_INVALID_FLAGS
-		"invalid role",	// ACTF_INVALID_ROLE
-		"non-existent base",	// ACTF_INVALID_BASE
-		"unsupported integer or float length",	// ACTF_UNSUPPORTED_LENGTH
-		"invalid encoding",	// ACTF_INVALID_ENCODING
-		"invalid environment",	// ACTF_INVALID_ENVIRONMENT
-		"invalid variant",	// ACTF_INVALID_VARIANT
-		"cycle offset greater than or equal to frequency",	// ACTF_CC_GTE_FREQ_ERROR
-		"non-existent field class alias is referred to",	// ACTF_NO_SUCH_ALIAS
-		"required property is missing",	// ACTF_MISSING_PROPERTY
-		"an extension is enabled which is not supported",	// ACTF_UNSUPPORTED_EXTENSION
-		"unknown clock origin",	// ACTF_NO_SUCH_ORIGIN
-		"default timestamp role specified but the data stream has no default clock",	// ACTF_NO_DEFAULT_CLOCK
-		"invalid \"metadata-stream-uuid\" role, no uuid in preamble or type?",	// ACTF_INVALID_UUID_ROLE
-		"invalid \"packet-magic-number\" role, should be first member in struct",	// ACTF_INVALID_MAGIC_ROLE
-		"top-level field class is not a struct when it should be",	// ACTF_NOT_A_STRUCT
-		"duplicate fragment, field class, name or id",	// ACTF_DUPLICATE_ERROR
-		"non-existent field class is referred to by id",	// ACTF_NO_SUCH_ID
-		"the CTF version is not supported",	// ACTF_UNSUPPORTED_VERSION
-		"no preamble",	// ACTF_NO_PREAMBLE
-		"a selection field or a length indicator field has the wrong type",	// ACTF_WRONG_FLD_TYPE
-		"field location not found",	// ACTF_MISSING_FLD_LOC
-		"not enough bits in the data stream",	// ACTF_NOT_ENOUGH_BITS
-		"the byte-order changed in the middle of a byte",	// ACTF_MID_BYTE_ENDIAN_SWAP
-		"a string has a length which is not compatible with its encoding",	// ACTF_INVALID_STR_LEN
-		"the packet magic number is incorrect",	// ACTF_MAGIC_MISMATCH
-		"the data stream UUID does not match the metadata UUID",	// ACTF_UUID_MISMATCH
-		"a selector field is not found for an optional or variant",	// ACTF_NO_SELECTOR_FLD
-		"a packet content length is larger than its total length",	// ACTF_INVALID_CONTENT_LEN
-		"a metadata packet is not valid",	// ACTF_INVALID_METADATA_PKT
+		"not an error", // ACTF_OK
+		"generic error", // ACTF_ERROR
+		"internal error", // ACTF_INTERNAL
+		"out of memory", // ACTF_OOM
+		"property was not found", // ACTF_NOTFOUND
+		"the json could not be parsed", // ACTF_JSON_PARSE_ERROR
+		"incompatible json value", // ACTF_JSON_ERROR
+		"incompatible type of json value", // ACTF_JSON_WRONG_TYPE
+		"a value is not greater or equal than zero", // ACTF_JSON_NOT_GTEZ
+		"a value is not greater than zero", // ACTF_JSON_NOT_GTZ
+		"an alignment is not a power of two", // ACTF_INVALID_ALIGNMENT
+		"non-existent byte order", // ACTF_INVALID_BYTE_ORDER
+		"non-existent bit order", // ACTF_INVALID_BIT_ORDER
+		"invalid range", // ACTF_INVALID_RANGE
+		"invalid range set", // ACTF_INVALID_RANGE_SET
+		"invalid uuid", // ACTF_INVALID_UUID
+		"invalid mapping", // ACTF_INVALID_MAPPING
+		"invalid field location", // ACTF_INVALID_FLD_LOC
+		"invalid field class", // ACTF_INVALID_FLD_CLS
+		"invalid flags", // ACTF_INVALID_FLAGS
+		"invalid role", // ACTF_INVALID_ROLE
+		"non-existent base", // ACTF_INVALID_BASE
+		"unsupported integer or float length", // ACTF_UNSUPPORTED_LENGTH
+		"invalid encoding", // ACTF_INVALID_ENCODING
+		"invalid environment", // ACTF_INVALID_ENVIRONMENT
+		"invalid variant", // ACTF_INVALID_VARIANT
+		"cycle offset greater than or equal to frequency", // ACTF_CC_GTE_FREQ_ERROR
+		"non-existent field class alias is referred to", // ACTF_NO_SUCH_ALIAS
+		"required property is missing", // ACTF_MISSING_PROPERTY
+		"an extension is enabled which is not supported", // ACTF_UNSUPPORTED_EXTENSION
+		"unknown clock origin", // ACTF_NO_SUCH_ORIGIN
+		"default timestamp role specified but the data stream has no default clock", // ACTF_NO_DEFAULT_CLOCK
+		"invalid \"metadata-stream-uuid\" role, no uuid in preamble or type?", // ACTF_INVALID_UUID_ROLE
+		"invalid \"packet-magic-number\" role, should be first member in struct", // ACTF_INVALID_MAGIC_ROLE
+		"top-level field class is not a struct when it should be", // ACTF_NOT_A_STRUCT
+		"duplicate fragment, field class, name or id", // ACTF_DUPLICATE_ERROR
+		"non-existent field class is referred to by id", // ACTF_NO_SUCH_ID
+		"the CTF version is not supported", // ACTF_UNSUPPORTED_VERSION
+		"no preamble", // ACTF_NO_PREAMBLE
+		"a selection field or a length indicator field has the wrong type", // ACTF_WRONG_FLD_TYPE
+		"field location not found", // ACTF_MISSING_FLD_LOC
+		"not enough bits in the data stream", // ACTF_NOT_ENOUGH_BITS
+		"the byte-order changed in the middle of a byte", // ACTF_MID_BYTE_ENDIAN_SWAP
+		"a string has a length which is not compatible with its encoding", // ACTF_INVALID_STR_LEN
+		"the packet magic number is incorrect", // ACTF_MAGIC_MISMATCH
+		"the data stream UUID does not match the metadata UUID", // ACTF_UUID_MISMATCH
+		"a selector field is not found for an optional or variant", // ACTF_NO_SELECTOR_FLD
+		"a packet content length is larger than its total length", // ACTF_INVALID_CONTENT_LEN
+		"a metadata packet is not valid", // ACTF_INVALID_METADATA_PKT
 	};
 	if (rc >= 0 && rc < (int) (sizeof(strs) / sizeof(*strs))) {
 		return strs[rc];
